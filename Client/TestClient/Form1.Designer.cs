055f5bfd33373a420244ce9160a757de205dec0c
Syudal <syudal@naver.com>
2023-12-21 15:48:16 +0900

    Create Basic GUI

--- Client/TestClient/Form1.Designer.cs
+++ Client/TestClient/Form1.Designer.cs
@@ -23,12 +23,126 @@
         ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            textBox3 = new TextBox();
+            label3 = new Label();
+            button2 = new Button();
+            textBox4 = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("맑은 고딕", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(29, 19);
+            label1.TabIndex = 0;
+            label1.Text = "IP :";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(47, 9);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(121, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("맑은 고딕", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(174, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(43, 19);
+            label2.TabIndex = 2;
+            label2.Text = "Port :";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(223, 9);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(62, 23);
+            textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(291, 9);
+            button1.Name = "button1";
+            button1.Size = new Size(106, 23);
+            button1.TabIndex = 4;
+            button1.Text = "Connect";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(59, 40);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(729, 23);
+            textBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("맑은 고딕", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(12, 42);
+            label3.Name = "label3";
+            label3.Size = new Size(41, 19);
+            label3.TabIndex = 6;
+            label3.Text = "Hex :";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(682, 9);
+            button2.Name = "button2";
+            button2.Size = new Size(106, 23);
+            button2.TabIndex = 7;
+            button2.Text = "Send";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(8, 70);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(780, 369);
+            textBox4.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox4);
+            Controls.Add(button2);
+            Controls.Add(label3);
+            Controls.Add(textBox3);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "TestClient";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private TextBox textBox2;
+        private Button button1;
+        private TextBox textBox3;
+        private Label label3;
+        private Button button2;
+        private TextBox textBox4;
     }
 }
