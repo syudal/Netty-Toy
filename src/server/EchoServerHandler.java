c4ee21ec31ff3cad2175e77b0a7f6d23f237c03a
Syudal <syudal@naver.com>
2023-12-21 13:21:21 +0900

    Update EchoServerHandler.java

--- src/server/EchoServerHandler.java
+++ src/server/EchoServerHandler.java
@@ -72,6 +72,7 @@ public class EchoServerHandler extends ChannelInboundHandlerAdapter {
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
         // Close the connection when an exception is raised.
         cause.printStackTrace();
+        channels.remove(ctx.channel());
         ctx.close();
     }
 
